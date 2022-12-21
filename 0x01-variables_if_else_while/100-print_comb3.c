1283d502718c40f3f8bbab115d84913151ac4f04
Favour Idowu <favouridowu02@gmail.com>
2022-12-21 04:33:32 -0800

    dpne

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible different
+ * combinations of two digits.
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+		int b;
+
+		for (b = 0; b <= 9; b++)
+			putchar((b % 10) + '0')
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
