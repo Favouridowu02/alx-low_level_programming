d818ef7d3662d945f81d762fe0975d9d88badeee
Favour Idowu <favouridowu02@gmail.com>
2022-12-21 02:25:09 -0800

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = (char)a;
+	for (a = 1; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar(\n);
+	return (0);
+}
