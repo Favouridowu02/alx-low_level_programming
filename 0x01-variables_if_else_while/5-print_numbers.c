ced149a6ceeb8b1c02106ff5bc5dffb29f5d01d3
Favour Idowu <favouridowu02@gmail.com>
2022-12-21 02:03:00 -0800

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d\n", a);
+		a++
+	}
+	return (0);
+}
