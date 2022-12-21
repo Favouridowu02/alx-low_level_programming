d021aeb1cbbda3574e3a9bdba2b06d75a3d54a2f
Favour Idowu <favouridowu02@gmail.com>
2022-12-21 04:16:45 -0800

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,11 @@ int main(void)
 	for (a = 0; a <= 9; a++)
 	{
 		putchar((a % 10) + '0');
-		if (a == 0)
+		if (a == 9)
 			break;
-		else {
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
 		}
-	}
 	putchar('\n');
 	return (0);
 }
