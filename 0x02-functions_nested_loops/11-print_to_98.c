9e1cf6c526ea9e5aa819b99866eaab8e17542b3b
Favour Idowu <favouridowu02@gmail.com>
2022-12-24 08:56:26 -0800

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,13 +7,10 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
-	{
-		if (n > 9)
-			_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
+	if (n > 9)
+		_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
 	_putchar('\n');
 }
