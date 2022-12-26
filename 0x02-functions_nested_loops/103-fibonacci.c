f56e32b01a2e94635226e2a02f3123a9f423454c
Favour Idowu <favouridowu02@gmail.com>
2022-12-26 02:42:51 -0800

    done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the first even fibonaci sequence
+ * that is less than 4000000
+ * Return: returns 0
+ */
+int main(void)
+{
+	long int i = 0;
+	long int j, k, sum;
+
+	j = 1;
+	k = 2;
+	sum = j + k;
+
+	do {
+		j = k;
+		k = sum
+		sum = j + k;
+		if ((sum % 2) == 0 || sum == 4000000)
+			i += sum;
+	}
+	while (sum <= 4000000)
+	printf("%ld", i);
+	return (0);
+}
