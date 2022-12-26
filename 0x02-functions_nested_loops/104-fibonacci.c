3ca59dc80420af5b2419e2cc5211c99566c14b0e
Favour Idowu <favouridowu02@gmail.com>
2022-12-26 05:40:37 -0800

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	unsigned long int j, k, sum;
+	unsigned long int fib1, fib2, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
