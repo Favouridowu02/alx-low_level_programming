dc7558a53a75fb26f84426dbc5a62f712d7dcaf3
Favour Idowu <favouridowu02@gmail.com>
2022-12-27 05:15:28 -0800

    done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -23,12 +23,6 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if ((year == 2000 || year == 2100) && day == 59 && month == 2)
-		{
-			day++;
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
