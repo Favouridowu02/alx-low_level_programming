e3db65cd9e309e71b4abc22651696dbb14467b84
Favour Idowu <favouridowu02@gmail.com>
2022-12-27 07:46:40 -0800

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * mul - Write a function that multiplies two integers.
  * @a: the first parameter
  * @b: the second parameter
- * Returns: returns the multiplication of a and b
+ * Return: returns the multiplication of a and b
  */
 int mul(int a, int b)
 {
