2303ab7a111508c7eea072eeb0154686659ce652
Favour Idowu <favouridowu02@gmail.com>
2022-12-29 07:24:00 -0800

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,19 +6,14 @@
  */
 void print_number(int n)
 {
-	if (n > 9 && n <= 99)
-		_putchar((n / 10) + '0');
-	if (n > 99 && n <= 999)
+	unsigned int i;
+
+	if (n < 0)
 	{
-		_putchar((n / 100) + '0');
-		_putchar((n / 10) % 10 + '0');
+		_putchar('-');
+		i = - * n;
 	}
-	if (n > 999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 '0');
-		_putchar((n / 10) % 10 + '0');
-	}
-	_putchar((n % 10) + '0');
-	_putchar('\n');
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
 }
