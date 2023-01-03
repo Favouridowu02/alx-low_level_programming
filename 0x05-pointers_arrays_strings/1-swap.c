56584cfc423a9956f88c42533457e16782e8f868
Favour Idowu <favouridowu02@gmail.com>
2023-01-03 04:28:00 -0800

    done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Write a function that swaps the values of two integers
+ * @a: the first parameter to be passed
+ * @b: the second parameter passed
+ */
+void swap_int(int *a, int *b)
+{
+	int sum;
+
+	sum = *a;
+	*a = *b;
+	*b = *a;
+}
