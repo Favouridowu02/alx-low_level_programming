15c4a0850afa245cb0bb52250ea5acb67582f5a0
Favour Idowu <favouridowu02@gmail.com>
2023-01-03 04:36:05 -0800

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @s: the string to be passed
+ * Return: returns len
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(s);
+	return (len);
+}
