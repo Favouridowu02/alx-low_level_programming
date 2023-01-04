92afbb469d32117f74efeb416847a1951eac2ccd
Favour Idowu <favouridowu02@gmail.com>
2023-01-04 06:58:41 -0800

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - Write a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
- * @s: the string parameter to be passed
+ * @str: the string parameter to be passed
  */
 void puts2(char *str)
 {
