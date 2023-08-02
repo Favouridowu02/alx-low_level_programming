04499b60936f041da0b6e985b6716dd12ed4f048
Favour Idowu <favouridowu02@gmail.com>
2023-08-02 17:10:43 -0600

    just starting

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - Entry point
+ * @dest: first parameter to ne passed
+ * @src: second paraeter to be passed
+ * Description: Write a function that concatenates two strings
+ * Return: (0)exit
+ */
+int char *_strcat(char *dest, char *src)
+{
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef Main.h
+#define Main.h
+
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif
