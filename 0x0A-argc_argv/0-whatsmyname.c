0181cc32e4f8479ca9cbf290bca5e732b19bf84e
Favour Idowu <favouridowu02@gmail.com>
2023-09-01 15:32:16 -0600

    Just finished the first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints its name, followed by a new line
+ * argc: arguments count
+ * argv: arguments vector
+ * Returns: Always 0.
+ */
+int main(int argc, char* argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
