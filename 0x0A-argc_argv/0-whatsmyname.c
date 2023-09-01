aec9e3c868a6a245ac99b62567ceff227affa94a
Favour Idowu <favouridowu02@gmail.com>
2023-09-01 15:45:26 -0600

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void)argc;
+	__attribute__((unused))argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
