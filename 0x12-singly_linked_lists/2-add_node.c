46924418980edd44387a29c857978b794aa2ad7b
Favour Idowu <favouridowu02@gmail.com>
2023-09-21 18:59:06 -0600

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,26 +10,30 @@ int _strlen(const char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
+	{
 		i++;
+	}
 	return (i);
 }
-/**
- * add_node_end - a function that adds a new node at the end of a list_t list.
- * @head: the link that head points too
- * @str: needs to be duplicated
- *
- * Return: the address of the new element, or NULL if it failed
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-	list_t *newNode;
 
-	newNode = malloc(sizeof(list_t));
-	if (newNode == NULL)
+/**
+ * add_node - add a new node at beginning of a list_t list.
+ * @head: head of a list_t list.
+ * @str: value to insert into element.
+ * Return: the number of nodes.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *add;
+
+	add = malloc(sizeof(list_t));
+	if (add == NULL)
 		return (NULL);
-	newNode->next = *head;
-	*head = newNode;
-	newNode->str = strdup(str);
-	newNode->len = _strlen(str);
-	return (newNode);
+	add->str = strdup(str);
+
+	add->len = _strlen(str);
+	add->next = *head;
+	*head = add;
+
+	return (add);
 }
