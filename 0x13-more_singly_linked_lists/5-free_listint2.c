be133101fc39ee1f791c043e763fb5206d4f80f6
Favour Idowu <favouridowu02@gmail.com>
2023-09-25 17:36:49 -0600

    task completed

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Write a function that frees a listint_t list
+ * @head: an instance of a node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *clean = *head;
+	listint_t *temp;
+
+	while (clean)
+	{
+		temp = clean->next;
+		free(clean);
+		clean = temp;
+	}
+	*head = NULL;
+}
