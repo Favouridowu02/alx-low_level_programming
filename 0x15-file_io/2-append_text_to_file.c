a022e9d11ee3b34ab8285a5f25880e4e43733b73
Favour Idowu <favouridowu02@gmail.com>
2023-10-03 08:30:23 -0600

    Another challenge conquered

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,11 +33,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		w = write(fd, text_content, _strlen(text_content));
-		if (w == -1)
-			return (-1);
-		close(fd);
-		return (-1);
 	}
 	close(fd);
+	if (w == -1)
+		return (-1);
 	return (1);
 }
