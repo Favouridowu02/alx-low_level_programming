4b16b5c188f2e333369ae81fc380a7e124163e9f
Favour Idowu <favouridowu02@gmail.com>
2023-10-03 06:32:07 -0600

    Just solevd this amazing task, To create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * strlen - returns the length of the string
+ * @text: the tring to check
+ * Return: the length of the string
+ */
+int _strlen(char *text)
+{
+	int i = 0;
+
+	while (text[i] != '\0')
+		i++;
+	return (i);
+}
+/**
  * create_file - a function that creates a file.
  * @filename: the name of the file to creat
  * @text_content: The text content to be inserted
@@ -14,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
-	w = write(fd, text_content, sizeof(text_content));
+	w = write(fd, text_content, _strlen(text_content));
 	if (fd == -1 || w == -1 || text_content == NULL)
 		return (-1);
 	close(fd);
