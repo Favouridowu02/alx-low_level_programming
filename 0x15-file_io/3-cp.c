15592216ea8d0a72ea425219b750e128218a1f79
Favour Idowu <favouridowu02@gmail.com>
2023-10-06 08:48:04 -0600

    Task solved

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,112 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int cp(const char *file_from, const char *file_to);
 /**
- * main - check the code
- * @ac: argument count
- * @av: argument vector
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
  *
- * Return:  Always 0
+ * Return: void
  */
-int main(int ac, char **av)
+void check97(int argc)
 {
-	if (ac != 3)
+	if (argc != 3)
 	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-		printf("Usage: cp file_from file_to\n");
 	}
-	cp(av[1], av[2]);
-	return (0);
+}
+/**
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
+}
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
 }
 
 /**
- * cp - a program that copies the content of a file to another file
- * @file_from: the file to copy from
- * @file_to: the file to copy to
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
  *
- * Return: returns 1 on success and -1 on failure
+ * Return: void
  */
-int cp(const char *file_from, const char *file_to)
+void check100(int check, int fd)
 {
-	int fd, fp, r, w, c, e;
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
 	char buffer[1024];
+	mode_t file_perm;
 
-	if (file_from == NULL)
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		exit(98);
-		printf("Error: Can't read from file %s\n", file_from);
-		return (-1);
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
-	fd = open(file_from, O_RDONLY | O_TRUNC);
-	fp = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	r = read(fd, buffer, 1024);
-	if (r == -1 || fd == -1)
-	{
-		exit(98);
-		printf("Error: Can't read from file %s\n", file_from);
-		return (-1);
-	}
-	w = write(fp, buffer, 1024);
-	if (fp == -1 || w == -1)
-	{
-		exit(99);
-		printf("Error: Can't write to %s", file_to);
-		close(fd);
-		return (-1);
-	}
-	c = close(fd);
-	e = close(fp);
-	if (c == -1)
-	{
-		exit(100);
-		printf("Error: Can't close fd %d", c);
-		return (-1);
-	}
-	if (e == -1)
-	{
-		exit(100);
-		printf("Error: Can't close fd %d", e);
-		return (-1);
-	}
-	return (1);
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
+	return (0);
 }
