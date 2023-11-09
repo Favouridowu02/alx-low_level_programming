fd6f72aed3a23534eaed5f51b73ead8aaa815655
Idowu Favour <favouridowu02@gmail.com>
2023-11-09 15:11:58 -0800

    Just finished task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,20 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h->prev != NULL)
-	{
-		h = h->prev;
-	}
-
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	printf("%d\n", h->n);
-	i++;
 	return (i);
 }
