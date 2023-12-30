2c4dad7d5463808a847165bc8072eaca0b37ebec
Favour Idowu <favouridowu02@gmail.com>
2023-12-30 05:01:33 +0100

    Just adding the main files

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		new->prev = NULL;
+/*		new->prev = NULL; */
 		*head = new;
 		return (new);
 	}
